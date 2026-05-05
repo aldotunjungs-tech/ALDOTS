02aa681d10d2823d3b1aac3b9289daa7ffdfc8c6
ALDO25550037 <aldotunjungs@gmail.com>
2026-05-05 21:34:54 +0700

    update

--- kategory_produk.php
+++ kategory_produk.php
@@ -67,7 +67,7 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
-                <span>My Profile</span>
+                <span>Profilku</span>
               </a>
             </li>
             <li>
