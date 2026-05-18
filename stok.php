bf361527dcb07cff2bd0c117d347b29ec744361c
ALDO25550037 <aldotunjungs@gmail.com>
2026-05-18 10:13:40 +0700

    mengubah kata

--- stok.php
+++ stok.php
@@ -480,7 +480,7 @@ if (isset($_POST['submit'])) {
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Aldo Tunjung s</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Aldo Tunjung</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
